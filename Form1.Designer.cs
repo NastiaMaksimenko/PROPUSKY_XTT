2289ef5cbf0cda69780a323c408eeed895c3416e
DarkAl550 <51300895+DarkAl550@users.noreply.github.com>
2020-02-11 14:31:08 +0300

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Main";
-            this.Text = "Form1";
+            this.Text = "XTT";
             this.ResumeLayout(false);
 
         }
